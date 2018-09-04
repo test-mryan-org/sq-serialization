4154b51adad09e35aa08da310c083cdada0ff0a0
tdeffaye <devnull@localhost>
2018-09-04 12:48:50 +0200

    make jaxrs work

--- json/jaxrs-support/src/test/java/com/swissquote/foundation/gson/model/Animal.java
+++
@@ -1,8 +0,0 @@
-package com.swissquote.foundation.gson.model;
-
-import com.swissquote.foundation.soa.support.api.annotations.GsonPolymorphic;
-
-@GsonPolymorphic
-public interface Animal {
-	String makeSound();
-}
