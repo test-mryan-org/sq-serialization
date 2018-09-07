5cbe6d65a69852dc3146331311042568612ed3b4
tdeffaye <devnull@localhost>
2018-09-07 17:08:23 +0200

    add message converter for spring amqp

--- json/spring-integration-support/src/main/java/com/swissquote/foundation/serialization/json/support/spring/integration/SQJsonObjectMapper.java
+++ json/spring-integration-support/src/main/java/com/swissquote/foundation/serialization/json/support/spring/integration/SQJsonObjectMapper.java
@@ -19,74 +19,74 @@ import lombok.extern.slf4j.Slf4j;
 @SuppressWarnings("unchecked")
 public class SQJsonObjectMapper<N, P> extends JsonObjectMapperAdapter<N, P> {
 
-	private volatile ClassLoader classLoader = ClassUtils.getDefaultClassLoader();
+    private volatile ClassLoader classLoader = ClassUtils.getDefaultClassLoader();
 
-	private JsonObjectMapper<N, P> jsonObjectMapper;
+    private final JsonObjectMapper<N, P> jsonObjectMapper;
 
-	public SQJsonObjectMapper() {
-		this(JsonSerialization.getSerializationProvider());
-	}
+    public SQJsonObjectMapper() {
+        this(JsonSerialization.getSerializationProvider());
+    }
 
-	public SQJsonObjectMapper(JsonSerializationProvider provider) {
-		this.jsonObjectMapper = provider.getJsonObjectMapper();
-	}
+    public SQJsonObjectMapper(JsonSerializationProvider provider) {
+        this.jsonObjectMapper = provider.getJsonObjectMapper();
+    }
 
-	public SQJsonObjectMapper(JsonObjectMapper<N, P> jsonObjectMapper) {
-		this.jsonObjectMapper = jsonObjectMapper;
-	}
+    public SQJsonObjectMapper(JsonObjectMapper<N, P> jsonObjectMapper) {
+        this.jsonObjectMapper = jsonObjectMapper;
+    }
 
-	@Override
-	public String toJson(Object value) throws Exception {
-		return jsonObjectMapper.toJson(value);
-	}
+    @Override
+    public String toJson(Object value) throws Exception {
+        return jsonObjectMapper.toJson(value);
+    }
 
-	@Override
-	public void toJson(Object value, Writer writer) throws Exception {
-		jsonObjectMapper.toJson(value, writer);
-	}
+    @Override
+    public void toJson(Object value, Writer writer) throws Exception {
+        jsonObjectMapper.toJson(value, writer);
+    }
 
-	@Override
-	public N toJsonNode(Object value) throws Exception {
-		return jsonObjectMapper.toJsonNode(value);
-	}
+    @Override
+    public N toJsonNode(Object value) throws Exception {
+        return jsonObjectMapper.toJsonNode(value);
+    }
 
-	@Override
-	public <T> T fromJson(Object json, Class<T> valueType) throws Exception {
-		return jsonObjectMapper.fromJson(json, valueType);
-	}
+    @Override
+    public <T> T fromJson(Object json, Class<T> valueType) throws Exception {
+        return jsonObjectMapper.fromJson(json, valueType);
+    }
 
-	@Override
-	public <T> T fromJson(Object json, Map<String, Object> javaTypes) throws Exception {
-		Class<?> classType = this.createJavaType(javaTypes, JsonHeaders.TYPE_ID);
-		Class<?> contentClassType = this.createJavaType(javaTypes, JsonHeaders.CONTENT_TYPE_ID);
-		Class<?> keyClassType = this.createJavaType(javaTypes, JsonHeaders.KEY_TYPE_ID);
+    @Override
+    public <T> T fromJson(Object json, Map<String, Object> javaTypes) throws Exception {
+        Class<?> classType = this.createJavaType(javaTypes, JsonHeaders.TYPE_ID);
+        Class<?> contentClassType = this.createJavaType(javaTypes, JsonHeaders.CONTENT_TYPE_ID);
+        Class<?> keyClassType = this.createJavaType(javaTypes, JsonHeaders.KEY_TYPE_ID);
 
-		if (keyClassType != null) {
-			log.warn("Gson doesn't support the Map 'key' conversion. Will be returned raw Map<String, Object>");
-			return (T) fromJson(json, Map.class);
-		}
+        if (keyClassType != null) {
+            log.warn("Gson doesn't support the Map 'key' conversion. Will be returned raw Map<String, Object>");
+            return (T) fromJson(json, Map.class);
+        }
 
-		if (contentClassType != null) {
-			log.warn("Gson doesn't support the Collection conversion. Will be returned raw Collection<Object>");
-			return (T) fromJson(json, Collection.class);
-		}
+        if (contentClassType != null) {
+            log.warn("Gson doesn't support the Collection conversion. Will be returned raw Collection<Object>");
+            return (T) fromJson(json, Collection.class);
+        }
 
-		return (T) fromJson(json, classType);
-	}
+        return (T) fromJson(json, classType);
+    }
 
-	@Override
-	public <T> T fromJson(P parser, Type valueType) throws Exception {
-		return jsonObjectMapper.fromJson(parser, valueType);
-	}
+    @Override
+    public <T> T fromJson(P parser, Type valueType) throws Exception {
+        return jsonObjectMapper.fromJson(parser, valueType);
+    }
 
-	private Class<?> createJavaType(Map<String, Object> javaTypes, String javaTypeKey) throws Exception {
-		Object classValue = javaTypes.get(javaTypeKey);
-		if (classValue instanceof Class<?>) {
-			return (Class<?>) classValue;
-		} else if (classValue != null) {
-			return ClassUtils.forName(classValue.toString(), this.classLoader);
-		} else {
-			return null;
-		}
-	}
+    private Class<?> createJavaType(Map<String, Object> javaTypes, String javaTypeKey) throws Exception {
+        Object classValue = javaTypes.get(javaTypeKey);
+        if (classValue instanceof Class<?>) {
+            return (Class<?>) classValue;
+        } else if (classValue != null) {
+            return ClassUtils.forName(classValue.toString(), this.classLoader);
+        } else {
+            return null;
+        }
+    }
 }
