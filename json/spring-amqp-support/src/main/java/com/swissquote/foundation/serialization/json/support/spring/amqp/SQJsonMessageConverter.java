c6088a7b23e7f14e3bd411fa88ad19304b6df015
tdeffaye <devnull@localhost>
2018-09-12 15:01:41 +0200

    improve spring-amqp-support

--- json/spring-amqp-support/src/main/java/com/swissquote/foundation/serialization/json/support/spring/amqp/SQJsonMessageConverter.java
+++ json/spring-amqp-support/src/main/java/com/swissquote/foundation/serialization/json/support/spring/amqp/SQJsonMessageConverter.java
@@ -1,93 +1,42 @@
 package com.swissquote.foundation.serialization.json.support.spring.amqp;
 
 import java.io.IOException;
-import java.util.Hashtable;
-import java.util.Map;
-import java.util.UUID;
+import java.lang.reflect.Type;
 
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConversionException;
 import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.util.ClassUtils;
 
-import com.swissquote.foundation.serialization.json.JsonObjectMapper;
-import com.swissquote.foundation.serialization.json.JsonSerialization;
-import com.swissquote.foundation.serialization.json.spi.JsonSerializationProvider;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.swissquote.foundation.serialization.json.JacksonJsonObjectMapper;
 
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class SQJsonMessageConverter<N, P> implements MessageConverter {
+public class SQJsonMessageConverter implements MessageConverter {
 
-	private final JsonObjectMapper<N, P> jsonObjectMapper;
+	@Setter
+	private Type type;
 
-	public static final String DEFAULT_CHARSET = "UTF-8";
-	public static final String DEFAULT_CLASSID_FIELD_NAME = "__TypeId__";
-	private static final String DEFAULT_HASHTABLE_TYPE_ID = "Hashtable";
-
-	private volatile Class<?> defaultHashtableClass = Hashtable.class;
-	private volatile String defaultCharset = DEFAULT_CHARSET;
-	private boolean createMessageIds = false;
-
-	public void setDefaultCharset(String defaultCharset) {
-		this.defaultCharset = defaultCharset;
-	}
-
-	public void setCreateMessageIds(boolean createMessageIds) {
-		this.createMessageIds = createMessageIds;
-	}
+	private final Jackson2JsonMessageConverter jackson2JsonMessageConverter;
+	private final ObjectMapper objectMapper;
 
 	public SQJsonMessageConverter() {
-		this(JsonSerialization.getSerializationProvider());
+		this(JacksonJsonObjectMapper.standardObjectMapper());
 	}
 
-	public SQJsonMessageConverter(JsonSerializationProvider provider) {
-		this.jsonObjectMapper = provider.getJsonObjectMapper();
-	}
-
-	public SQJsonMessageConverter(JsonObjectMapper<N, P> jsonObjectMapper) {
-		this.jsonObjectMapper = jsonObjectMapper;
+	public SQJsonMessageConverter(ObjectMapper objectMapper) {
+		this.objectMapper = objectMapper;
+		jackson2JsonMessageConverter = new Jackson2JsonMessageConverter(objectMapper);
 	}
 
 	@Override
 	public Message toMessage(Object object, MessageProperties messageProperties) throws MessageConversionException {
-		if (messageProperties == null) {
-			messageProperties = new MessageProperties();
-		}
-		Message message = createMessage(object, messageProperties);
-		messageProperties = message.getMessageProperties();
-		if (createMessageIds && messageProperties.getMessageId() == null) {
-			messageProperties.setMessageId(UUID.randomUUID().toString());
-		}
-		return message;
-	}
-
-	private Message createMessage(Object objectToConvert, MessageProperties messageProperties)
-			throws MessageConversionException {
-		byte[] bytes;
-		try {
-			String jsonString = jsonObjectMapper.toJson(objectToConvert);
-			bytes = jsonString.getBytes(defaultCharset);
-		}
-		catch (IOException e) {
-			throw new MessageConversionException("Failed to convert Message content", e);
-		}
-		messageProperties.setContentType(MessageProperties.CONTENT_TYPE_JSON);
-		messageProperties.setContentEncoding(defaultCharset);
-		if (bytes != null) {
-			messageProperties.setContentLength(bytes.length);
-		}
-		messageProperties.getHeaders().put(DEFAULT_CLASSID_FIELD_NAME, fromClass(objectToConvert.getClass()));
-
-		return new Message(bytes, messageProperties);
-	}
-
-	private Object fromClass(Class<?> aClass) {
-		if (Map.class.isAssignableFrom(aClass)) {
-			return DEFAULT_HASHTABLE_TYPE_ID;
-		}
-		return aClass.getName();
+		return jackson2JsonMessageConverter.toMessage(object, messageProperties);
 	}
 
 	@Override
@@ -99,11 +48,22 @@ public class SQJsonMessageConverter<N, P> implements MessageConverter {
 			if (contentType != null && contentType.contains("json")) {
 				String encoding = properties.getContentEncoding();
 				if (encoding == null) {
-					encoding = defaultCharset;
+					encoding = jackson2JsonMessageConverter.getDefaultCharset();
 				}
 				try {
-					Class<?> targetClass = toClass(message.getMessageProperties());
-					content = jsonObjectMapper.fromJson(new String(message.getBody(), encoding), targetClass);
+					if (type != null) {
+						JavaType targetJavaType =
+								objectMapper.getTypeFactory().constructType(type);
+						content = convertBytesToObject(message.getBody(), encoding, targetJavaType);
+					} else if (jackson2JsonMessageConverter.getClassMapper() == null) {
+						JavaType targetJavaType =
+								jackson2JsonMessageConverter.getJavaTypeMapper().toJavaType(message.getMessageProperties());
+						content = convertBytesToObject(message.getBody(), encoding, targetJavaType);
+					} else {
+						Class<?> targetClass =
+								jackson2JsonMessageConverter.getClassMapper().toClass(message.getMessageProperties());
+						content = convertBytesToObject(message.getBody(), encoding, targetClass);
+					}
 				}
 				catch (IOException e) {
 					throw new MessageConversionException("Failed to convert Message content", e);
@@ -118,27 +78,13 @@ public class SQJsonMessageConverter<N, P> implements MessageConverter {
 		return content;
 	}
 
-	private Class<?> toClass(MessageProperties properties) {
-		Map<String, Object> headers = properties.getHeaders();
-		Object classIdFieldNameValue = headers.get(DEFAULT_CLASSID_FIELD_NAME);
-		String classId = null;
-		if (classIdFieldNameValue != null) {
-			classId = classIdFieldNameValue.toString();
-		}
-		if (classId == null) {
-			throw new MessageConversionException("failed to convert Message content. Could not resolve "
-					+ DEFAULT_CLASSID_FIELD_NAME + " in header and no defaultType provided");
-		}
-
-		if (classId.equals(DEFAULT_HASHTABLE_TYPE_ID)) {
-			return this.defaultHashtableClass;
-		}
-		try {
-			return ClassUtils.forName(classId, getClass().getClassLoader());
-		}
-		catch (ClassNotFoundException | LinkageError e) {
-			throw new MessageConversionException("failed to resolve class name [" + classId + "]", e);
-		}
+	private Object convertBytesToObject(byte[] body, String encoding, JavaType targetJavaType) throws IOException {
+		String contentAsString = new String(body, encoding);
+		return objectMapper.readValue(contentAsString, targetJavaType);
 	}
 
+	private Object convertBytesToObject(byte[] body, String encoding, Class<?> targetClass) throws IOException {
+		String contentAsString = new String(body, encoding);
+		return objectMapper.readValue(contentAsString, objectMapper.constructType(targetClass));
+	}
 }
---
+++ json/spring-amqp-support/src/test/java/com/swissquote/foundation/serialization/json/support/spring/amqp/Complex.java
@@ -0,0 +1,15 @@
+package com.swissquote.foundation.serialization.json.support.spring.amqp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Complex {
+
+	private String name;
+	private int count;
+
+}
